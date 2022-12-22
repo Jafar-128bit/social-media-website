2b775a2cf8dc6e20b9d17f34a33bd64e36f9e74b
Jafar Iqbal Choudhury <jafariqbalchoudhury@gmail.com>
2022-12-22 22:26:12 +0530

    State management System Setup
    Setup the redux library for state management in website.

--- src/state/state.js
+++ src/state/state.js
@@ -1,8 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 
 /*This is the state that will be stored in the globule state,
 * this data is going to be accessible in entire application
 * and can be used anywhere in the application*/
 const initialState = {
-    
-}
+    mode: "light",
+    user: null,
+    token: null,
+    post: []
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout : (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends non-existent :( ")
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            state.posts = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+        }
+    }
+})
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
